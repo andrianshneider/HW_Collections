7978bf4f5e0734725981bc788bf1d1d32ae762b4
andrianshneider <ashneider@gmail.com>
2024-04-26 09:56:43 +0300

    completed HW 26.04.2024

--- Collections/Program.cs
+++ Collections/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 internal class Program
 {
@@ -46,27 +47,41 @@ internal class Program
             string name;
             float rating;
             bool input;
+            bool stop;
 
             Console.WriteLine("Выполняется Задача 2 (Dictionary):");
-                        
+
+            //цикл по добавлению записей в словарь
             do
             {
-                Console.WriteLine("Введите имя студента для добавления в базу (имя не должно быть пустым):");
-                name = Console.ReadLine();
-            }
-            while (name == "");
+                do
+                {
+                    Console.WriteLine("Введите имя студента для добавления в базу (имя не должно быть пустым):");
+                    name = Console.ReadLine();
+                }
+                while (name == "");
 
-            do
-            {
-                Console.WriteLine("Введите его среднюю оценку (от 2 до 5):");
-                input = float.TryParse(Console.ReadLine(), out var value);
-                rating = value;
-            }
-            while (!input | rating < 2 | rating > 5);
-                                               
-            DictHW.Add(name, rating);
-            Console.WriteLine("Имя студента и средняя оценка добавлены!");
+                do
+                {
+                    Console.WriteLine("Введите его среднюю оценку (от 2 до 5):");
+                    input = float.TryParse(Console.ReadLine(), out var value);
+                    rating = value;
+                }
+                while (!input | rating < 2 | rating > 5);
 
+                DictHW.Add(name, rating);
+                Console.WriteLine("Имя студента и средняя оценка добавлены!");
+
+                Console.WriteLine("");
+                Console.WriteLine("Введите 1, чтобы ввести следующую запись, другое значения для прекращения ввода:");
+
+                var a = Console.ReadLine();
+
+                if (a == "1") { stop = false; } else { stop = true; }
+            }
+            while (!stop);
+
+            //вывод получившейся коллекции
             Console.WriteLine("");
             Console.WriteLine("Результирующий список:");
 
@@ -74,15 +89,57 @@ internal class Program
             {
                 Console.WriteLine("Имя = {0}, Средняя оценка = {1}", kvp.Key, kvp.Value);
             }
+
+            //поиск и вывод средней оценки по имени студента
+            Console.WriteLine("");
+            Console.WriteLine("Введите имя студента для поиска и вывода его средней оценки:");
+
+            var findName = Console.ReadLine();
+
+            if (DictHW.TryGetValue(findName, out var findRating))
+            { Console.WriteLine($"Средняя оценка студента {findName}: {findRating}"); }
+            else { Console.WriteLine($"Студента с именем {findName} не существует!"); }
+
         }
     }
 
     //приватный класс для Задания 3 (двусвязный список)
     private class Task3_DoubleLinkedList
     {
+        public LinkedList<string> DoubleLinkedList = new LinkedList<string>();
+
         public void TaskLoop()
         {
             Console.WriteLine("Выполняется Задача 3 (Double Linked List):");
+
+            Console.WriteLine("");
+            Console.WriteLine("Введите значения для добавления в двусвязный список:");
+
+            for (int i = 0; i < 3; i++)
+            {
+                var element = Console.ReadLine();
+                DoubleLinkedList.AddFirst(element);
+            }
+
+            //вывод результата
+            Console.WriteLine("");
+            Console.WriteLine("Результирующий список:");
+
+            foreach (var item in DoubleLinkedList)
+            {
+                Console.WriteLine(item);
+            }
+
+            //вывод результата в обратном порядке
+            Console.WriteLine("");
+            Console.WriteLine("Результирующий список (в обратном порядке):");
+
+            var currentNode = DoubleLinkedList.Last;
+            while (currentNode != null)
+            {
+                Console.WriteLine(currentNode.Value);
+                currentNode = currentNode.Previous;
+            }
         }
     }
 
